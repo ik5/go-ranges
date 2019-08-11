c736e68c4f9a3e55d9678306bb71d9b254850d92
Ido Kanner <idok@linesip.com>
2019-08-11 13:18:07 +0300

    Initial commit

---
+++ ranges.go
@@ -0,0 +1,51 @@
+package ranges
+
+// InRangeInt check to see if a given number is in a given range
+func InRangeInt(value, from, to int) bool {
+	return value >= from && value <= to
+}
+
+// InRangeUint check to see if a given number is in a given range
+func InRangeUint(value, from, to uint) bool {
+	return value >= from && value <= to
+}
+
+// InRangeInt8 check to see if a given number is in a given range
+func InRangeInt8(value, from, to int8) bool {
+	return value >= from && value <= to
+}
+
+// InRangeUint8 check to see if a given number is in a given range
+func InRangeUint8(value, from, to uint8) bool {
+	return value >= from && value <= to
+}
+
+// InRangeInt16 check to see if a given number is in a given range
+func InRangeInt16(value, from, to int16) bool {
+	return value >= from && value <= to
+}
+
+// InRangeUint16 check to see if a given number is in a given range
+func InRangeUint16(value, from, to uint16) bool {
+	return value >= from && value <= to
+}
+
+// InRangeInt32 check to see if a given number is in a given range
+func InRangeInt32(value, from, to int32) bool {
+	return value >= from && value <= to
+}
+
+// InRangeUint32 check to see if a given number is in a given range
+func InRangeUint32(value, from, to uint32) bool {
+	return value >= from && value <= to
+}
+
+// InRangeInt64 check to see if a given number is in a given range
+func InRangeInt64(value, from, to int64) bool {
+	return value >= from && value <= to
+}
+
+// InRangeUint64 check to see if a given number is in a given range
+func InRangeUint64(value, from, to uint64) bool {
+	return value >= from && value <= to
+}
